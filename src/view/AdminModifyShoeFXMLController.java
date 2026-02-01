61d9a6e136a1fdd2aeef752bb1a2679dc4674552
alicia-2004 <aliciaclasegradosuperior@gmail.com>
2026-02-01 12:25:16 +0100

    JavaDoc alicia (admin)

--- src/view/AdminModifyShoeFXMLController.java
+++ src/view/AdminModifyShoeFXMLController.java
@@ -37,14 +37,28 @@ import model.Profile;
 import model.Shoe;
 
 /**
- * FXML Controller class
+ * Controller class for the Admin Modify Shoe view.
+ * <p>
+ * This JavaFX controller manages the administration interface that allows an
+ * administrator to view, add, delete and modify shoe data. It also provides
+ * access to profile modification and help documentation.
+ * </p>
  *
  * @author Deusto
+ * @version 1.0
+ * @since 2024
  */
 public class AdminModifyShoeFXMLController implements Initializable {
 
+    /**
+     * TableView that displays the list of shoes.
+     */
     @FXML
     private TableView<Shoe> tableShoe;
+
+    /**
+     * Table columns for shoe attributes.
+     */
     @FXML
     private TableColumn<Shoe, Integer> columnBrand;
     @FXML
@@ -57,14 +71,36 @@ public class AdminModifyShoeFXMLController implements Initializable {
     private TableColumn<Shoe, Integer> columnPrice;
     @FXML
     private TableColumn<Shoe, Integer> columnStock;
+
+    /**
+     * Button used to delete a selected shoe.
+     */
     @FXML
     private Button deleteButton;
-    
-     private PersonalLogger personalLogger;
 
+    /**
+     * Logger used to register application errors.
+     */
+    private PersonalLogger personalLogger;
+
+    /**
+     * Observable list containing the shoes displayed in the table.
+     */
     private ObservableList<Shoe> listShoe;
+
+    /**
+     * Main application controller.
+     */
     private Controller cont;
+
+    /**
+     * Profile of the currently logged-in user.
+     */
     private Profile profile;
+
+    /**
+     * Menu bar and menu items of the view.
+     */
     @FXML
     private MenuBar menu;
     @FXML
@@ -80,26 +116,53 @@ public class AdminModifyShoeFXMLController implements Initializable {
     @FXML
     private MenuItem menuReport;
 
-
+    /**
+     * Gets the main controller instance.
+     *
+     * @return the controller
+     */
     public Controller getCont() {
         return cont;
     }
 
+    /**
+     * Sets the main controller and loads the shoe data.
+     *
+     * @param cont the controller to set
+     */
     public void setCont(Controller cont) {
         this.cont = cont;
         loadShoes();
     }
 
+    /**
+     * Gets the current user profile.
+     *
+     * @return the user profile
+     */
     public Profile getProfile() {
         return profile;
     }
 
+    /**
+     * Sets the current user profile.
+     *
+     * @param profile the profile to set
+     */
     public void setProfile(Profile profile) {
         this.profile = profile;
     }
 
     /**
      * Initializes the controller class.
+     * <p>
+     * This method is automatically called after the FXML file has been loaded.
+     * It configures the table columns, enables editing, and sets up stock
+     * modification handling.
+     * </p>
+     *
+     * @param url the location used to resolve relative paths
+     * @param rb the resources used to localize the root object
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -120,11 +183,17 @@ public class AdminModifyShoeFXMLController implements Initializable {
 
     }
 
+    /**
+     * Loads all shoes from the controller and displays them in the table.
+     */
     private void loadShoes() {
         listShoe.setAll(cont.loadShoes());
         tableShoe.setItems(listShoe);
     }
 
+    /**
+     * Deletes the selected shoe from the system and removes it from the table.
+     */
     @FXML
     private void delete() {
         Shoe selectedShoe = tableShoe.getSelectionModel().getSelectedItem();
@@ -133,6 +202,10 @@ public class AdminModifyShoeFXMLController implements Initializable {
             listShoe.remove(selectedShoe);
         }
     }
+
+    /**
+     * Opens a modal window to add a new shoe to the system.
+     */
     @FXML
     public void addNew() {
         try {
@@ -158,6 +231,14 @@ public class AdminModifyShoeFXMLController implements Initializable {
         }
     }
 
+    /**
+     * Handles stock value editing from the table.
+     * <p>
+     * Prevents negative values and updates the stock through the controller.
+     * </p>
+     *
+     * @param event the edit event containing the new stock value
+     */
     private void onStockEdit(
             TableColumn.CellEditEvent<Shoe, Integer> event
     ) {
@@ -172,7 +253,12 @@ public class AdminModifyShoeFXMLController implements Initializable {
         }
         cont.updateStockShoe(shoe, newStock);
     }
-    
+
+    /**
+     * Opens the window that allows the user to modify their profile.
+     *
+     * @param event the action event triggered by the menu item
+     */
     @FXML
     private void openModifyProfileWindow(ActionEvent event) {
         try {
@@ -194,7 +280,12 @@ public class AdminModifyShoeFXMLController implements Initializable {
             personalLogger.logError(ex.getMessage());
         }
     }
-    
+
+    /**
+     * Opens the user manual PDF file using the system default PDF viewer.
+     *
+     * @param event the action event triggered by the menu item
+     */
     @FXML
     private void openReport(ActionEvent event) {
         try {
@@ -208,6 +299,11 @@ public class AdminModifyShoeFXMLController implements Initializable {
     private void openManual(ActionEvent event) {
     }
 
+    /**
+     * Displays an information alert with a custom message.
+     *
+     * @param msg the message to display
+     */
     private void showInfo(String msg) {
         Alert a = new Alert(Alert.AlertType.INFORMATION);
         a.setTitle("Error modifying stock");
